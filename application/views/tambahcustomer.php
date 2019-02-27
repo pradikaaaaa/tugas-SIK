6757c57de2e1fa20dbed19fc31be87c7f7a0a997
diosyahrizal <diosyahrizal@gmail.com>
2019-02-27 14:27:07 +0700

    customer insert

--- application/views/tambahcustomer.php
+++ application/views/tambahcustomer.php
@@ -4,24 +4,24 @@
     <div class="container">
         <div class="panel panel-default">
             <div class="panel-body">
-                <?php echo form_open_multipart('');?>
+                <?php echo form_open_multipart('Customer/index');?>
 				<?php echo validation_errors(); ?>
 
                 <div class="form-group">
 					<label for="" >ID Customer :  </label>
-					<input type="text" name="id_barang" class="form-control" id="id_barang" value="" readonly>
+					<input type="text" name="id_customer" class="form-control" id="id_barang" value="<?= $id  ?>" readonly>
 				</div>
                 <div class="form-group">
                     <label for="">Nama Customer :</label>
-                    <input class="form-control" type="text" name="nama_barang" id="">
+                    <input class="form-control" type="text" name="nama_customer" id="">
                 </div>
                 <div class="form-group">
                     <label>Alamat</label>
-                    <textarea name="deskripsi" class="form-control" id="ckeditor1"></textarea>
+                    <textarea name="alamat" class="form-control" id="ckeditor1"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="">Telepon :</label>
-                    <input class="form-control" type="text" name="harga" id="">
+                    <input class="form-control" type="text" name="telepon" id="">
                 </div>
 
                 <button type="submit" class="btn btn-primary">Tambah</button>
@@ -35,9 +35,7 @@
 <?php $this->load->view('footer');?>
 <script src="<?=base_url()?>assets/ckeditor/ckeditor.js"></script>
 <script type="text/javascript">
-    $(function(){
-        CKEDITOR.replace('ckeditor1');
-    });
+
 </script>
 
 
